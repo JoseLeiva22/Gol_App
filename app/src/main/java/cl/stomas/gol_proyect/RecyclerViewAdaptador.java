7030bffd5b61b774551587af565607af3f6e99af
JoseLeiva22 <71997205+JoseLeiva22@users.noreply.github.com>
2020-10-06 12:27:21 -0300

    actualizacion del proyecto en android

--- app/src/main/java/cl/stomas/gol_proyect/RecyclerViewAdaptador.java
+++ app/src/main/java/cl/stomas/gol_proyect/RecyclerViewAdaptador.java
@@ -40,7 +40,7 @@ public class RecyclerViewAdaptador extends RecyclerView.Adapter<RecyclerViewAdap
         ViewHolder viewHolder=new ViewHolder(view);
         return viewHolder;
     }
-
+    
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.nombre.setText(ListaCanchas.get(position).getNombre());
